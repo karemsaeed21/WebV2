e7e8939cca15038590fdb5d7e3b86390d6048c88
karemsaeed21 <109914324+karemsaeed21@users.noreply.github.com>
2024-05-22 10:50:21 +0300

    Add files via upload

--- Js/kar.js
+++ Js/kar.js
@@ -1,6 +1,17 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+function toggleNav() {
+  var navWidth = document.getElementById("mySidenav").style.width;
+  if (navWidth === "0px" || navWidth === "") {
+    document.getElementById("mySidenav").style.width = "250px";
+  } else {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+  document.getElementsByClassName("main-btn")[0].addEventListener("mouseover", function () {
+      this.innerHTML = "Click on me";
+    });
+  document.getElementsByClassName("main-btn")[0].addEventListener("mouseout", function () {
+      this.innerHTML = "Who We Are";
+    });
